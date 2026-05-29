f3cc95fedf4d015eefc9d64b4debc69e8729595a
AyushAJS <jangidayush89@gmail.com>
2026-05-29 17:17:15 +0530

    layer optimization concept is added in the docker file

--- docker-learnings/src/index.ts
+++ docker-learnings/src/index.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response, NextFunction } from 'express';
+import dotenv from 'dotenv';
 
 const app = express();
+
+dotenv.config();
 
 app.get('/', (req: Request, res: Response, next: NextFunction)=>{
 	res.status(200).json({
@@ -12,4 +15,7 @@ app.get('/', (req: Request, res: Response, next: NextFunction)=>{
 
 app.listen(5050, ()=> {
 	console.log("Server is running on PORT:5050 🚀🚀🚀");
-})
+});
+
+	// console.log("Environment Variable: ", process.env.DATABASE_URL);
+	// We can add the environment variable using <<< docker run -p localPORT:dockerPORT -e DATABASE_URL="" dockerIMAGE >>>
